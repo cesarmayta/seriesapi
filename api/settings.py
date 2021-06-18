f9e13382bdba22610a39244d557a836a60daa98a
= <cmayta@tecsup.edu.pe>
2021-06-18 01:45:19 -0500

    creando conexion para postgresql

--- api/settings.py
+++ api/settings.py
@@ -78,12 +78,21 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import dj_database_url
+from decouple import config
+
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+    
 }
 
 
