3fb9ec116a8dbc39e9392924973b6646b6278f1c
= <cmayta@tecsup.edu.pe>
2021-06-18 19:00:31 -0500

    comentamos media url

--- api/settings.py
+++ api/settings.py
@@ -136,9 +136,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
+#MEDIA_ROOT = BASE_DIR / 'media'
 
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
